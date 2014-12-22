fef470b4fb298915191111451b80379c4e77de42
Lewis Goddard <lewisgoddard@eustasy.org>
2014-12-22 10:53:46 +0000

    Update header.php

--- chunks/header.php
+++ chunks/header.php
@@ -33,6 +33,7 @@
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 		ga('create', 'UA-45667989-9', 'devede.org');
+		ga('require', 'displayfeatures');
 		ga('send', 'pageview');
 	</script>
 
